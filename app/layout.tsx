298f46fb7eb520ad35b68f5242a86c6ea29cd48e
xyy <xyy@example.com>
2023-11-18 16:29:20 +0800

    next learn first commit
    Signed-off-by: xyy <xyy@example.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { noto_sans } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body  className={`${noto_sans.className} antialiased`}>{children}</body>
     </html>
   );
 }
